e262d4bdd583a7b718b5c1363744954ad0d4d2dd
orcworker1 <160320755+orcworker1@users.noreply.github.com>
2025-11-11 12:31:37 +0300

    update model

--- purch_calculator/removal_rates/models.py
+++ purch_calculator/removal_rates/models.py
@@ -3,24 +3,36 @@ from django.db import models
 
 
 class RemovalForSunflower(models.Model):
-    Moisture = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    Weed_impurity = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    Oil_impurity = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    Oil_content = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    KCHM = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    Protein = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    moisture_base = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    moisture_removal = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    weed_impurity_base = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    weed_impurity_removal = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    oil_impurity_removal = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    oil_impurity_base = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    oil_content_base = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    oil_content_removal = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    KCHM_base = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    KCHM_removal = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    protein_base = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    protein_removal = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
 
     def __str__(self):
         return f"Removal #{self.id}"
 
 class RemovalForRapeseed(models.Model):
-    Moisture = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    Weed_impurity = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    Oil_impurity = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    Oil_content = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    KCHM = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    Protein = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    moisture_base = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    moisture_removal = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    weed_impurity_base = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    weed_impurity_removal = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    oil_impurity_removal = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    oil_impurity_base = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    oil_content_base = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    oil_content_removal = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    KCHM_base = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    KCHM_removal = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    protein_base = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    protein_removal = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return f"Removal #{self.id}"
