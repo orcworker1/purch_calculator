74e54713d1a2ce96848e05972de2f393494ad77f
orcworker1 <160320755+orcworker1@users.noreply.github.com>
2025-11-17 14:29:36 +0300

    update for render

--- purch_calculator/settings.py
+++ purch_calculator/settings.py
@@ -10,22 +10,30 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-651_$+ne=7p0t4_63b(#cn^2gfph@8qtx8u4q(825bixglqh_#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['webserver',
+    '127.0.0.1',
+    'localhost',
+    '0.0.0.0',
+    'https://purch-calculator.onrender.com',
+     ]
 
 
 # Application definition
